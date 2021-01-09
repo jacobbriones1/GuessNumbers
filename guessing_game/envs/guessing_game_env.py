8c28819afc245ef593496024dd56705297d7c6c3
Jacob Briones <70331998+jacobbriones1@users.noreply.github.com>
2021-01-09 15:09:13 -0700

    Add files via upload

--- guessing_game/envs/guessing_game_env.py
+++ guessing_game/envs/guessing_game_env.py
@@ -2,7 +2,8 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
-
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 
 class GuessingGameEnv(gym.Env):
     metadata = {'render.modes':['human']}
@@ -30,18 +31,19 @@ class GuessingGameEnv(gym.Env):
                 self.action = action
         
         self.observation = self.MSE(action)
-        
+        self.history.append(self.observation)
         if self.observation < 0.01:
-            done = False
-            self.guess_count +=1
-            reward = 0
-        else:
             done = True
             self.guess_count +=1
-            reward =1
+            reward = 1
+        else:
+            done = False
+            self.guess_count +=1
+            reward =0
             
 
         return self.observation,reward, done, { "numbers":self.numbers, "guesses":self.guess_count}
+
     
     def reset(self, size=3):
         self.size = size
@@ -49,13 +51,45 @@ class GuessingGameEnv(gym.Env):
             [self.np_random.uniform(-self.range, self.range) for i in range(self.size)])
         
         self.guess_count = 0
-        self.observation = 0
+        self.observation = self.MSE([0]*size)
+        self.history = [self.observation]
         return self.observation
-    
+
+
+            
     def render(self, mode='human', close=False):
-        print('Error: ', self.observation)
+        #creating a subplot
+        
+        if plt.gcf()== None:
+            fig,ax = plt.subplot(figsize=(10,10))
+        else:
+            fig = plt.gcf()
+            ax = plt.gca()
+        plt.ion()
+        
+        
+        xs = [i for i in range(self.guess_count)]
+        ys = self.history[:self.guess_count]
+        ax.clear()
+        ax.plot(xs, ys,'-o')
+
+        plt.xlim([-0.01,self.guess_count])
+        plt.xlabel('Time')
+        plt.ylabel('error')
+        plt.title('MSE of guess')
+
+        plt.show()
+        
+        
     
     def MSE(self, guess):
         return np.sum((self.numbers-guess)**2)/self.size
     
     
+if __name__=='__main__':
+    import guessing_game
+    import gym
+    env = gym.make('NumberGuessing-v0')
+    env.reset(3)
+    env.step([0,0,0])
+    env.render()
